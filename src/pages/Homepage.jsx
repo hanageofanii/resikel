af2505809477191638878fb6acb904b6e1a4cbe8
hanageofani <hanasucianindageofani@gmail.com>
2024-11-03 13:52:02 +0700

    edit Homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-
 const Homepage = () => {
   return (
     <main>
-      <section className="bg-green-100 text-center py-16 px-8 lg:px-24 flex items-center justify-center">
+      <section className="bg-green-200 text-center py-16 px-8 lg:px-24 flex items-center justify-center">
         <div className="flex-1 text-left max-w-lg mx-auto space-y-4">
-          <h1 className="text-3xl font-bold text-green-600">
+          <h1 className="text-3xl font-bold text-black-600">
             Yuk Kurangi Sampahmu Hari Ini!
           </h1>
-          <p className="text-xl text-green-600">
+          <p className="text-xl text-black-600">
             Dan Dapatkan Reward untuk Setiap Sampah yang Kamu Berikan kepada
             Kami!
           </p>
-          <button className="mt-4 bg-orange-500 text-white py-2 px-4 rounded hover:bg-orange-600">
-            Ayo Kurangi Sampah!
-          </button>
+          <a href="/Rewards">
+            <button className="mt-4 bg-orange-400 text-white py-2 px-4 rounded-full hover:bg-orange-500">
+              Ayo Kurangi Sampah!
+            </button>
+          </a>
         </div>
         <div className="flex-1 mt-8 max-w-sm mx-auto">
           <img
@@ -37,35 +37,37 @@ const Homepage = () => {
             memudahkan kamu untuk mendaur ulang. Temukan cara-cara kreatif untuk
             mengurangi sampah dan dapatkan reward menarik!
           </p>
-          <button className="mt-4 bg-orange-500 text-white py-2 px-4 rounded hover:bg-orange-600">
-            Explore More
-          </button>
+          <a href="/About">
+            <button className="mt-4 bg-orange-400 text-white py-2 px-4 rounded-full hover:bg-orange-500">
+              Explore More
+            </button>
+          </a>
         </div>
       </section>
-      <section className="bg-green-100 text-center py-16">
-        <h1 className="text-2xl font-bold text-green-600 mb-8">
+      <section className="bg-green-200 text-center py-16">
+        <h1 className="text-2xl font-bold text-black-600 mb-8">
           Bangun Masa Depan yang Bersih!
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 max-w-4xl mx-auto">
           <div className="bg-white p-6 rounded-lg shadow-md">
-            <i className="fas fa-dollar-sign text-green-600 text-3xl mb-4"></i>
+            <i className="fas fa-dollar-sign text-black-300 text-3xl mb-4"></i>
             <h2 className="font-bold">Investasi untuk Masa Depan</h2>
             <p>Kelola sampah dengan baik untuk generasi mendatang.</p>
           </div>
           <div className="bg-white p-6 rounded-lg shadow-md">
-            <i className="fas fa-lightbulb text-green-600 text-3xl mb-4"></i>
+            <i className="fas fa-lightbulb text-black-300 text-3xl mb-4"></i>
             <h2 className="font-bold">Inovasi Tanpa Batas!</h2>
             <p>Temukan solusi-solusi kreatif untuk masalah sampah.</p>
           </div>
           <div className="bg-white p-6 rounded-lg shadow-md">
-            <i className="fas fa-leaf text-green-600 text-3xl mb-4"></i>
+            <i className="fas fa-leaf text-black-300 text-3xl mb-4"></i>
             <h2 className="font-bold">Lingkungan Lestari</h2>
             <p>Jaga bumi agar tetap indah dan sehat.</p>
           </div>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4 justify-center max-w-3xl mx-auto">
           <div className="bg-white p-6 rounded-lg shadow-md">
-            <i className="fas fa-globe text-green-600 text-3xl mb-4"></i>
+            <i className="fas fa-globe text-black-300 text-3xl mb-4"></i>
             <h2 className="font-bold">Peduli Bumi</h2>
             <p>
               Membantu melindungi alam dan menciptakan masa depan yang lebih
@@ -73,25 +75,27 @@ const Homepage = () => {
             </p>
           </div>
           <div className="bg-white p-6 rounded-lg shadow-md">
-            <i className="fas fa-info-circle text-green-600 text-3xl mb-4"></i>
+            <i className="fas fa-info-circle text-black-300 text-3xl mb-4"></i>
             <h2 className="font-bold">Hidup Harmonis dengan Alam</h2>
             <p>Hidup berdampingan dengan alam tanpa merusak.</p>
           </div>
         </div>
       </section>
       <section className="bg-white-200 text-center py-16 flex justify-center">
-        <div className="bg-green-100 rounded-lg shadow-lg p-8 max-w-md">
-          <h2 className="text-2xl font-bold text-green-600">
+        <div className="bg-green-200 rounded-lg shadow-lg p-8 max-w-md">
+          <h2 className="text-2xl font-bold text-black-600">
             Siap untuk Belajar Lebih Dalam?
           </h2>
-          <p className="mt-4 text-gray-700">
+          <p className="mt-4 text-black-300">
             Mulailah dengan mengikuti program edukasi kami yang dirancang untuk
             memberikan pengetahuan mendalam tentang pengelolaan sampah dan daur
             ulang.
           </p>
-          <button className="mt-4 bg-orange-500 text-white py-2 px-4 rounded hover:bg-orange-600">
-            Explore More
-          </button>
+          <a href="/Education">
+            <button className="mt-4 bg-orange-400 text-white py-2 px-4 rounded-full hover:bg-orange-500">
+              Explore More
+            </button>
+          </a>
         </div>
       </section>
     </main>
