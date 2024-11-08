a472db9a3662cf3ef09b9b6b2d572b451c5e58cf
hanageofani <hanasucianindageofani@gmail.com>
2024-11-08 11:45:05 +0700

    edit fixed header

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -13,74 +13,85 @@ const Navbar = () => {
   };
 
   return (
-    <header className="bg-green-200 py-4">
-      <div className="container mx-auto px-6 flex justify-between items-center max-w-6xl">
-        {/* Logo */}
-        <div className="flex items-center">
-          <a href="/">
-            <img
-              src="./src/assets/images/logo.png"
-              alt="Logo Resikel"
-              className="w-11 h-11 mr-3"
-            />
-          </a>
+    <>
+      <header className="bg-green-200 py-4 fixed top-0 w-full z-50">
+        <div className="container mx-auto px-6 flex justify-between items-center max-w-6xl">
+          {/* Logo */}
+          <div className="flex items-center">
+            <a href="/">
+              <img
+                src="./src/assets/images/logo.png"
+                alt="Logo Resikel"
+                className="w-11 h-11 mr-3"
+              />
+            </a>
+          </div>
+
+          {/* Hamburger Icon */}
+          <div className="block lg:hidden">
+            <button onClick={toggleMenu} className="text-black">
+              <i
+                className={`fas ${isOpen ? "fa-times" : "fa-bars"} text-2xl`}
+              />
+            </button>
+          </div>
+
+          {/* Navigation Links */}
+          <nav className={`lg:flex ${isOpen ? "block" : "hidden"} lg:block`}>
+            <ul className="flex flex-col lg:flex-row lg:space-x-6 space-y-4 lg:space-y-0">
+              <li>
+                <a href="/" className="text-black-600 hover:text-green-800">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/About"
+                  className="text-black-600 hover:text-green-800">
+                  About Us
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/Education"
+                  className="text-black-600 hover:text-green-800">
+                  Education
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/Rewards"
+                  className="text-black-600 hover:text-green-800">
+                  Rewards
+                </a>
+              </li>
+
+              {/* Login Link inside the menu (visible on mobile) */}
+              <li className="lg:hidden">
+                <a
+                  href="/Login"
+                  className="text-black-600 cursor-pointer hover:text-green-800">
+                  Login
+                </a>
+              </li>
+            </ul>
+          </nav>
+
+          {/* Login Section for larger screens */}
+          <div className="hidden lg:flex items-center space-x-2">
+            <i className="fas fa-user text-green-600"></i>
+            <a
+              href="/Login"
+              className="text-black-600 cursor-pointer hover:text-green-800">
+              Login
+            </a>
+          </div>
         </div>
+      </header>
 
-        {/* Hamburger Icon */}
-        <div className="block lg:hidden">
-          <button onClick={toggleMenu} className="text-black">
-            <i className={`fas ${isOpen ? "fa-times" : "fa-bars"} text-2xl`} />
-          </button>
-        </div>
-
-        {/* Navigation Links */}
-        <nav className={`lg:flex ${isOpen ? "block" : "hidden"} lg:block`}>
-          <ul className="flex flex-col lg:flex-row lg:space-x-6 space-y-4 lg:space-y-0">
-            <li>
-              <a href="/" className="text-black-600 hover:text-green-800">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="/About" className="text-black-600 hover:text-green-800">
-                About Us
-              </a>
-            </li>
-            <li>
-              <a
-                href="/Education"
-                className="text-black-600 hover:text-green-800">
-                Education
-              </a>
-            </li>
-            <li>
-              <a
-                href="/Rewards"
-                className="text-black-600 hover:text-green-800">
-                Rewards
-              </a>
-            </li>
-
-            {/* Login Link inside the menu (visible on mobile) */}
-            <li className="lg:hidden">
-              <a
-                href="/Login"
-                className="text-black-600 cursor-pointer hover:text-green-800">
-                Login
-              </a>
-            </li>
-          </ul>
-        </nav>
-
-        {/* Login Section for larger screens */}
-        <div className="hidden lg:flex items-center space-x-2">
-          <i className="fas fa-user text-green-600"></i>
-          <a
-            href="/Login"
-            className="text-black-600 cursor-pointer hover:text-green-800"></a>
-        </div>
-      </div>
-    </header>
+      {/* Main Content */}
+      <main className="pt-16">{/* Your main content goes here */}</main>
+    </>
   );
 };
 
