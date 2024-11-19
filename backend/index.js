34bc6f600c5f6c3acb05ae3c85eeb93984c8a9ef
MuhRafiii <mrafi0603@gmail.com>
2024-11-19 22:33:12 +0700

    create backend

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+import cors from "cors";
+import express from "express";
+import UserRoute from "./routes/UserRoute.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(UserRoute);
+
+app.listen(5000, () => console.log("Server up and running..."));
