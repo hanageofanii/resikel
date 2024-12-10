b4710708fc0e5036da0aa4342bdc113cc01a5ce7
Elpratama <elpratama1403@gmail.com>
2024-12-10 23:15:33 +0700

    Mengubah halaman admin dan menambah header halaman admin

--- frontend/src/component/Sidebar.jsx
+++ frontend/src/component/Sidebar.jsx
@@ -19,27 +19,47 @@ const Sidebar = () => {
   if (isAuthPage) return null;
 
   return (
-    <div className="fixed top-0 left-0 h-screen w-64 bg-green-200 text-black shadow-lg">
-      <div className="p-6 border-b border-green-300">
-        <h2 className="text-2xl font-bold mb-4 text-center">Admin Dashboard</h2>
+    <div>
+      <div className="w-screen t-0  h-20 bg-white text-slate-800 flex justify-end">
+        <div className="flex items-center">
+          <img
+            src="./src/assets/images/logo.png"
+            alt="Illustration of mission"
+            className="mx-auto w-16"
+          />
+        </div>
+        <div className="text-black flex justify-center flex-col mr-10">
+          <p className="text-lg font-semibold">Resikel</p>
+          <p className="font-medium text-sm">Admin</p>
+        </div>
       </div>
-      <ul className="p-4 space-y-4">
-        {[
-          { path: "/articleadmin", label: "Articles" },
-          { path: "/contactadmin", label: "Contact" },
-          { path: "/sampahadmin", label: "Sampah" },
-          { path: "/useradmin", label: "Users" },
-        ].map((item, index) => (
-          <li key={index} className="rounded-lg hover:bg-green-300 transition">
-            <Link
-              to={item.path}
-              className="block p-3 text-lg font-semibold rounded-lg text-center hover:text-black"
+      <div className="fixed top-0 left-0 h-screen w-64 bg-white text-slate-800">
+        <div className="p-6 border-b">
+          <h2 className="text-2xl font-bold mb-4 text-center">
+            Admin Dashboard
+          </h2>
+        </div>
+        <ul className="p-4 space-y-4">
+          {[
+            { path: "/articleadmin", label: "Articles" },
+            { path: "/contactadmin", label: "Contact" },
+            { path: "/sampahadmin", label: "Sampah" },
+            { path: "/useradmin", label: "Users" },
+          ].map((item, index) => (
+            <li
+              key={index}
+              className="rounded-lg hover:bg-slate-200 transition"
             >
-              {item.label}
-            </Link>
-          </li>
-        ))}
-      </ul>
+              <Link
+                to={item.path}
+                className="block p-3 text-lg font-semibold rounded-lg text-center hover:text-black"
+              >
+                {item.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
